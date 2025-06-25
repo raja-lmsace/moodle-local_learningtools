fc67839bb2604d393b71dcee9c44edd711ac286b
raja <raja.lmsace@gmail.com>
2025-06-25 17:54:15 +0530

    BugFix: html writter moodle 44 compatibility issue fixed.

--- ltool/note/lib.php
+++ ltool/note/lib.php
@@ -22,7 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use core\output\html_writer;
+use html_writer;
 use core_user\output\myprofile\tree;
 defined('MOODLE_INTERNAL') || die();
 
